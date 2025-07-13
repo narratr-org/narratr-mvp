9e9a038f112bebd47de5e700ca78b3eb6dfb7c50
edmonddantesj <kikithewitch3@gmail.com>
2025-07-14 00:39:28 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 1) 서버리스 함수 크기를 대폭 줄여 주는 스탠드얼론 모드
+  output: 'standalone',
+
+  // 2) 기존 별칭 설정 유지
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
